75a24bae0d94d9d1e2c4e762c3e1ed5d74ac2c42
Marlon Andrei <marlon@micrologi.com.br>
2019-11-17 23:02:30 -0300

    Remoção do node processfilter

--- system/process.js
+++ system/process.js
@@ -5,38 +5,87 @@ module.exports = function(RED) {
         var node = this;
 
         node.on('input', function(msg) {
-            
+
             const si = require('systeminformation');
-            if (config.option == "1") {
-                si.currentLoad()
-                .then(data => {
-                    msg.payload = data;
-                    node.send(msg);
-                })
-                .catch(error => console.error(error));
-            } else if (config.option == "2") {
-                si.fullLoad()
-                .then(data => {
-                    msg.payload = data;
-                    node.send(msg);
-                })
-                .catch(error => console.error(error));
-            } else if (config.option == "3") {
+/*
+            console.log(msg.payload);
+            //Se informado payload, então filtra processos desejados
+            if ((msg.payload != undefined) && (msg.payload!="")) {
+
+                var filterprocesses = msg.payload;
+                var foundprocess = [];
+
+                //Se for uma lista de string, transforma para array
+                if (!Array.isArray(filterprocesses)) {
+                    filterprocesses = filterprocesses.split(",");
+                }
+
                 si.processes()
-                .then(data => {
-                    msg.payload = data;
+                .then(dataproc => {
+
+                    for (cont=0;cont<dataproc.list.length;cont++) {
+
+                        for (cont2=0;cont2<filterprocesses.length;cont2++) {
+                            
+                            pos = dataproc.list[cont].name.toLowerCase().indexOf(filterprocesses[cont2].toLowerCase());
+                            
+                            if (pos > -1) {
+
+                                var processo = {
+                                    name: dataproc.list[cont].name,
+                                    pcpu: dataproc.list[cont].pcpu,
+                                    pmem: dataproc.list[cont].pmem,
+                                    started: dataproc.list[cont].started,
+                                    user: dataproc.list[cont].user,
+                                    path: dataproc.list[cont].path
+                                }
+
+                                foundprocess.push(processo);
+                            }
+                        }
+
+
+                    }
+                
+                    msg.payload = foundprocess;
                     node.send(msg);
                 })
                 .catch(error => console.error(error));
-            } else if (config.option == "4") {
-                si.services('*')
-                .then(data => {
-                    msg.payload = data;
-                    node.send(msg);
-                })
-                .catch(error => console.error(error));
+
             } else {
-            }
+*/
+                if (config.option == "1") {
+                    si.currentLoad()
+                    .then(data => {
+                        msg.payload = data;
+                        node.send(msg);
+                    })
+                    .catch(error => console.error(error));
+                } else if (config.option == "2") {
+                    si.fullLoad()
+                    .then(data => {
+                        msg.payload = data;
+                        node.send(msg);
+                    })
+                    .catch(error => console.error(error));
+                } else if (config.option == "3") {
+                    si.processes()
+                    .then(data => {
+                        msg.payload = data;
+                        node.send(msg);
+                    })
+                    .catch(error => console.error(error));
+                } else if (config.option == "4") {
+                    si.services('*')
+                    .then(data => {
+                        msg.payload = data;
+                        node.send(msg);
+                    })
+                    .catch(error => console.error(error));
+                } else {
+                }
+    
+            //}
             
         });
     };
